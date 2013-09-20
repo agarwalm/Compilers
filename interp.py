5d7168df95f1279fe4594dbea3d398ac0eb29f29
ccg8e <christinegraff@Christines-MacBook-Pro.local>
2013-09-20 15:35:33 +0200

    is able to interpret simple one line arithmetic

--- interp.py
+++ interp.py
@@ -9,3 +9,43 @@ ast = compiler.parseFile(filePath)
 
 print ast
 
+#make a dictionary of variables
+varD = {}
+
+def interpret(n):
+
+	if isinstance(n, Module):
+		return interpret(n.node)
+
+	elif isinstance(n, Stmt):
+		for node in n:
+			print "NODE:"
+			print node
+			return interpret(node)
+
+	elif isinstance(n, Discard):
+		return interpret(n.expr)
+
+	elif isinstance(n, Assign):
+		if not isinstance(n.nodes[0], AssName) or len(n.nodes)>1:
+			print "ERROR! assign to only one variable please"
+			return
+		print n.nodes[0].name
+		varD[n.nodes[0].name] = interpret(n.expr)
+
+	elif isinstance(n, Add):
+		return interpret(n.left) + interpret(n.right)
+
+	elif isinstance(n, Sub):
+		return interpret(n.left) - interpret(n.right)
+
+	elif isinstance(n, Const):
+		return n.value
+	
+
+
+interpret(ast)
+print varD
+
+		
+
