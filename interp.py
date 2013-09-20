bccdf69e43582fb4129495d5784d760e82aaa5f5
ccg8e <christinegraff@Christines-MacBook-Pro.local>
2013-09-20 16:10:22 +0200

    everything we did on Friday  with '#' will be ignored, and an empty message aborts the commit.

--- interp.py
+++ interp.py
@@ -15,22 +15,19 @@ varD = {}
 def interpret(n):
 
 	if isinstance(n, Module):
-		return interpret(n.node)
+		 return interpret(n.node)
 
 	elif isinstance(n, Stmt):
 		for node in n:
-			print "NODE:"
-			print node
-			return interpret(node)
+			interpret(node)
 
-	elif isinstance(n, Discard):
+	if isinstance(n, Discard):
 		return interpret(n.expr)
 
 	elif isinstance(n, Assign):
 		if not isinstance(n.nodes[0], AssName) or len(n.nodes)>1:
 			print "ERROR! assign to only one variable please"
 			return
-		print n.nodes[0].name
 		varD[n.nodes[0].name] = interpret(n.expr)
 
 	elif isinstance(n, Add):
@@ -41,8 +38,22 @@ def interpret(n):
 
 	elif isinstance(n, Const):
 		return n.value
+
+	elif isinstance(n, Printnl):
+		
+		if len(n.nodes)>1:
+			print "ERROR! print can have only one constant integer argument."
+			return
+		print interpret(n.nodes[0])
+
+	elif isinstance(n, Name):
+		return varD[n.name]
+
+			
+	
 	
 
+		
 
 interpret(ast)
 print varD
